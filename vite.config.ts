ff79db52e683f2c40cbe90e68ef0eac55fa19d10
merry <k.maria.m.24.05@gmail.com>
2024-01-18 20:33:08 +0200

    finish project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/data-table-app/",
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      api: "/src/api",
+      assets: "/src/assets",
+      components: "/src/components",
+      constants: "/src/constants",
+      contexts: "/src/contexts",
+      locales: "/src/locales",
+      types: "/src/types",
+      utils: "/src/utils",
+    },
+  },
+});
